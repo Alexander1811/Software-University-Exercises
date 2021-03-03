09772dc09d19af98c66d0b2cf04acab1f9cdcdce
a_alexandrov1811@outlook.com <a_alexandrov1811@outlook.com>
2021-03-03 20:27:03 +0200

    commit

---
+++ C# OOP - February 2021/Inheritance - Exercise/05. Restaurant/Coffee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Restaurant
+{
+    public class Coffee : HotBeverage
+    {
+        private const double DefaultMililiters = 50;
+        private const decimal DefaultPrice = 3.5M;
+
+        public Coffee(string name, double caffeine) 
+            : base(name, DefaultPrice, DefaultMililiters)
+        {
+            this.Caffeine = caffeine;
+        }
+
+        public double Caffeine { get; set; }
+    }
+}
