df588bfa96cad7655d984f0c8e7b6f11611d1d8e
Alexander <a_alexandrov1811@outlook.com>
2022-02-05 19:53:45 +0200

    commit

---
+++ 09. C# Web Basics - January 2022/03. Web Server - State Management/BasicWebServer.Server/HTTP/Session.cs
@@ -0,0 +1,36 @@
+﻿namespace BasicWebServer.Server.HTTP
+{
+    using BasicWebServer.Server.Common;
+    using System.Collections.Generic;
+
+    public class Session
+    {
+        public const string SessionCookieName = "MyWebServerSID";
+        public const string SessionCookieDateKey = "CurrentDate";
+        public const string SessionUserKey = "AuthenticatedUserId";
+
+        private Dictionary<string, string> data;
+
+        public Session(string id)
+        {
+            Guard.AgainstNull(id, nameof(id));
+
+            this.Id = id;
+            this.data = new Dictionary<string, string>();
+        }
+
+        public string this[string key]
+        {
+            get => this.data[key];
+            set => this.data[key] = value;
+        }
+
+        public string Id { get; init; }
+
+        public bool ContainsKey(string key) 
+            => this.data.ContainsKey(key);
+
+        public void Clear()
+            => this.data.Clear();
+    }
+}
---
+++ 09. C# Web Basics - January 2022/03. Web Server - State Management/BasicWebServer.Server/Responses/TextResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace BasicWebServer.Server.Responses
+{
+    using BasicWebServer.Server.HTTP;
+    using System;
+
+    public class TextResponse : ContentResponse
+    {
+        public TextResponse(string text,
+            Action<Request, Response> preRenderAction = null)
+            : base(text, ContentType.PlainText, preRenderAction)
+        {
+        }
+    }
+}
